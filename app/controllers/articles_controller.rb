b6bba82c3d285a0c7246f39c51fdff825f0dbb65
Thinus <thinusplanb97@gmail.com>
2021-01-14 20:06:03 +0200

    Add the rest of the crud operations and link pages

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,4 +8,37 @@ class ArticlesController < ApplicationController
     @articles = Article.all
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(params.require(:article).permit(:title, :description))
+      flash[:notice] = "Article was edited successfully"
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+
+  
+  def destroy
+    @article = Article.find(params[:id])
+    if @article.destroy
+      flash[:notice] = "Article was edited successfully"
+    end
+    redirect_to articles_path
+  end
+
+  def create
+    @article = Article.new(params.require(:article).permit(:title, :description))
+    if @article.save
+      flash[:notice] = "Article was created successfully"
+      redirect_to @article
+    else
+      render 'new'
+    end
+  end
+
 end
