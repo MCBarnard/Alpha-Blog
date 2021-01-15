441f15ece43f8f00df55076f17a51c7295779434
Thinus <thinusplanb97@gmail.com>
2021-01-15 11:15:49 +0200

    Add Users table and model

---
+++ app/models/user.rb
@@ -0,0 +1,4 @@
+class User < ApplicationRecord
+  # validates :username, presence: true, length: { minimum: 6, maximum: 32 }
+  # validates :email, presence: true, length: { minimum: 4, maximum: 50 }
+end
